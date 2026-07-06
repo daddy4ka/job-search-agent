28c658578bf5fa8704c0713463f8176aef9858e7
daddy4ka <158582218+daddy4ka@users.noreply.github.com>
2026-07-06 23:29:54 +0200

    feat: wire up pmgroup scraper

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from scrapers.companies import (
     scrape_temabit,
     scrape_evoplay, scrape_headway, scrape_griddynamics, scrape_avenga, scrape_betterme,
     scrape_obrio, scrape_allstarsit, scrape_autodoc, scrape_whitebit, scrape_ideals,
-    scrape_genesis, scrape_uklon, scrape_macpaw, scrape_readdle, scrape_upstars, scrape_dripify,
+    scrape_genesis, scrape_uklon, scrape_macpaw, scrape_readdle, scrape_upstars, scrape_dripify, scrape_pmgroup,
 )
 from tracker import filter_new, mark_seen
 from notifier import send_job, send_summary
@@ -61,6 +61,7 @@ SCRAPERS = {
     "readdle":        scrape_readdle,
     "upstars":        scrape_upstars,
     "dripify":        scrape_dripify,
+    "pmgroup":        scrape_pmgroup,
     # UA job boards
     "dou":            scrape_dou,
     "djinni":         scrape_djinni,
